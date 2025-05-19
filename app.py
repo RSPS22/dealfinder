6224fd3792855e718cbd84d1feb544c2b890906f
RSPS22 <rspsinvestments@gmail.com>
2025-05-19 15:38:58 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ def upload():
         properties_df['ARV'] = properties_df['Listing Price'] * 1.1
         properties_df['Offer Price'] = properties_df.apply(
             lambda row: min(row['ARV'] * 0.65, row['Listing Price'] * 0.95), axis=1
-        )
 
 def is_high_potential(row):
     if pd.notnull(row['Offer Price']) and pd.notnull(row['Listing Price']):
@@ -60,7 +59,6 @@ def is_high_potential(row):
         properties_df['High Potential'] = properties_df.apply(is_high_potential, axis=1)
             lambda row: row['Offer Price'] < row['Listing Price'] * 0.8,
             axis=1
-        )
 
         for i, row in properties_df.iterrows():
             filename = f"LOI_{row['Id']}.docx"
